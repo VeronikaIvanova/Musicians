e7915703d9e517e5f5d73b39faabe7442737ec95
Veronika Ivanova <veronikaiva95@gmail.com>
2017-06-19 03:39:11 +0300

    user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+          :remberable 
 belongs_to :city
 belongs_to :state
 belongs_to :country
